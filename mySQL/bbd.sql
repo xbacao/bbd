3eea3a9b4fb30f776629b67e4057ba796be9a23f
Joao Ribeiro <joaolribeiro@ua.pt>
2016-12-29 02:25:54 +0000

    added get new schedule stored procedure

--- mySQL/bbd.sql
+++ mySQL/bbd.sql
@@ -1,46 +1,60 @@
+
+DROP SCHEMA IF EXISTS bbd;
 CREATE SCHEMA IF NOT EXISTS bbd;
 USE bbd ;
 
-DROP TABLE IF EXISTS bbd.valve;
 CREATE TABLE IF NOT EXISTS bbd.valve (
   valveID INT NOT NULL,
   description VARCHAR(100),
   PRIMARY KEY (valveID)
-)
+);
 
-DROP TABLE IF EXISTS bbd.schedule;
 CREATE TABLE IF NOT EXISTS bbd.schedule (
   scheduleID INT NOT NULL AUTO_INCREMENT,
-  valveID INT NOT NULL,
+  valveID_f INT NOT NULL,
   description VARCHAR(100),
-  sent BIT NOT NULL DEFAULT 0,
+  sent BIT(1) NOT NULL DEFAULT 0,
   created_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
-  PRIMARY KEY (scheduleID),
-  CONSTRAINT fk_valve FOREIGN KEY valveID REFERENCES bbd.valve (valveID) ON DELETE CASCADE)
-)
+  PRIMARY KEY (scheduleID)
+);
+ALTER TABLE bbd.schedule ADD FOREIGN KEY fk_valve(valveID_f) REFERENCES bbd.valve(valveID)
+ON DELETE CASCADE;
 
-DROP TABLE IF EXISTS bbd.schedule_entry;
 CREATE TABLE IF NOT EXISTS bbd.schedule_entry (
   entryID INT NOT NULL AUTO_INCREMENT,
-  scheduleID INT NOT NULL,
+  scheduleID_f INT NOT NULL,
   start_time TIME NOT NULL,
   stop_time TIME NOT NULL,
-  PRIMARY KEY (entryID),
-  CONSTRAINT fk_schedule FOREIGN KEY scheduleID REFERENCES bbd.schedule (scheduleID) ON DELETE CASCADE)
-)
+  PRIMARY KEY (entryID)
+);
+ALTER TABLE bbd.schedule_entry ADD FOREIGN KEY fk_schedule(scheduleID_f) REFERENCES bbd.schedule (scheduleID)
+ON DELETE CASCADE;
 
 DELIMITER $$
-DROP PROCEDURE IF EXISTS get_valve_schedule(IN valveID INT)
+DROP PROCEDURE IF EXISTS get_valve_schedule;
+CREATE PROCEDURE get_valve_schedule(IN valveID INT)
 BEGIN
-	SELECT @sche_id := MAX(scheduleID) FROM (SELECT * FROM bbd.schedule WHERE valveID=valveID) as T1;
-  SELECT @sent := MAX(sent) FROM bbd.schedule WHERE scheduleID=@sche_id;
-  IF  @sent = 0 THEN
-    SELECT * FROM bbd.schedule_entry WHERE scheduleID=@sche_id;
-  END IF;
-	
+  SELECT *
+  FROM bbd.schedule_entry INNER JOIN (
+    SELECT scheduleID
+    FROM bbd.schedule INNER JOIN (
+      SELECT @sche_id := MAX(scheduleID) 
+      FROM bbd.schedule 
+      WHERE valveID_f=valveID) AS T1 ON scheduleID=@sche_id
+    WHERE sent=0) AS T2 ON scheduleID_f=scheduleID;
 END $$
 DELIMITER ;
 
+/*
+--TEST PROC--
+INSERT INTO bbd.valve VALUES(1,"asdf");
+INSERT INTO bbd.schedule(valveID_f, description) VALUES (1, "AAAAA");
+INSERT INTO bbd.schedule_entry(scheduleID_f, start_time, stop_time) VALUES(1, CURTIME(),CURTIME());
+INSERT INTO bbd.schedule_entry(scheduleID_f, start_time, stop_time) VALUES(1, CURTIME(),CURTIME());
+INSERT INTO bbd.schedule_entry(scheduleID_f, start_time, stop_time) VALUES(1, CURTIME(),CURTIME());
+INSERT INTO bbd.schedule_entry(scheduleID_f, start_time, stop_time) VALUES(1, CURTIME(),CURTIME());
 
+CALL get_valve_schedule(1);
+*/
 
 
--- mySQL/upload_db.sh
+++ mySQL/upload_db.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+S_PATH=`dirname $(readlink -f $0)`
+
 echo "uploading sql script..."
-scp mySQL/bbd.sql f0lk@139.59.170.48:/home/f0lk/bbd_server
+scp $S_PATH/bbd.sql f0lk@178.62.6.44:/opt/bbd_server/mysql
 if [ $? != 0 ]
 then
 	echo "An error has occured uploading!"
