5ce1d0dfc6fe2bcbe935384d7ca007c9da5d5710
Joao Ribeiro <joaolribeiro@av.it.pt>
2017-04-08 13:52:45 +0100

    del bins

--- arduino/bin/socket_bbd.d
+++
@@ -1,5 +0,0 @@
-bin/socket_bbd.cpp.o bin/socket_bbd.S bin/socket_bbd.d: \
- /home/jlr/Documents/bbd/arduino/lib/socket_bbd/socket_bbd.cpp \
- /home/jlr/Documents/bbd/arduino/lib/socket_bbd/socket_bbd.h
-
-/home/jlr/Documents/bbd/arduino/lib/socket_bbd/socket_bbd.h:
