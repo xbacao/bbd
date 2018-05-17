600d3157be2a75f342d206f9aec0ed814fd75c8a
xbacao <jlribeiro99@gmail.com>
2018-05-17 17:04:17 +0100

    added scheduler thread execution code

--- client/daemon/src/schedule/schedule.h
+++ client/daemon/src/schedule/schedule.h
@@ -2,12 +2,14 @@
 #define _SCHEDULE_H_
 
 #include <stdint.h>
+#include <stdbool.h>
 
 struct schedule{
 	uint16_t schedule_id;
 	uint16_t valve_id;
 	uint16_t start;
 	uint16_t stop;
+	bool is_active;
 };
 
 #endif
--- client/daemon/src/schedule_manager/schedule_manager.c
+++ client/daemon/src/schedule_manager/schedule_manager.c
@@ -4,51 +4,84 @@
 #include <pthread.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include "log/log.h"
 
-static struct schedule* device_schedules;
-static bool schedules_set=false;
+#define SCHEDULER_SLEEP_INTERVAL      100000 //us
 
-static pthread_mutex_t* schedules_mtx;
-static pthread_t* scheduler_thread;
+static struct schedule* _device_schedules;
+static uint16_t _schedules_len;
+static bool _schedules_set=false;
+
+static pthread_mutex_t* _schedules_mtx;
+static pthread_t* _scheduler_thread;
 
 static void* _sm_run_scheduler_thread();
 
 static void* _sm_run_scheduler_thread(){
-  while(true){
-    //TODO
-  }
+  time_t rawtime;
+  struct tm * timeinfo;
+  uint16_t curr_mins;
 
+  while(true){
+    time(&rawtime);
+    timeinfo=localtime(&rawtime);
+    curr_mins=timeinfo->tm_hour*60+timeinfo->tm_min;
+
+    for(uint16_t i=0;i<_schedules_len;i++){
+      
+      //activate schedule
+      if(!_device_schedules[i].is_active && _device_schedules[i].start <= curr_mins &&
+          _device_schedules[i].stop >= curr_mins){
+        pthread_mutex_lock(_schedules_mtx);
+        vm_set_valve_state(_device_schedules[i].valve_id, VALVE_OPEN);
+        _device_schedules[i].is_active=true;
+        pthread_mutex_unlock(_schedules_mtx);
+      }
+
+      //deactivate schedule
+      if(_device_schedules[i].is_active && (curr_mins < _device_schedules[i].start ||
+          curr_mins > _device_schedules[i].stop)){
+        pthread_mutex_lock(_schedules_mtx);
+        vm_set_valve_state(_device_schedules[i].valve_id, VALVE_CLOSED);
+        _device_schedules[i].is_active=false;
+        pthread_mutex_unlock(_schedules_mtx);
+      }
+
+      usleep(SCHEDULER_SLEEP_INTERVAL);
+    }
+  }
   return NULL;
 }
-
 
 //FIXME
 void sm_set_new_schedules(struct schedule* sches, uint16_t schedules_len){
   uint16_t sches_bytesize=sizeof(struct schedule)*schedules_len;
 
-  pthread_mutex_lock(schedules_mtx);
+  //TODO ADD CHECK IF SCHEDULES ARE VALID
 
-  if(schedules_set){
-    free(device_schedules);
+  pthread_mutex_lock(_schedules_mtx);
+
+  if(_schedules_set){
+    free(_device_schedules);
   }
 
-  device_schedules=(struct schedule*) malloc(sches_bytesize);
+  _device_schedules=(struct schedule*) malloc(sches_bytesize);
+  memcpy(_device_schedules, sches, sches_bytesize);
+  _schedules_len=schedules_len;
 
-  memcpy(device_schedules, sches, sches_bytesize);
-
-  pthread_mutex_unlock(schedules_mtx);
+  pthread_mutex_unlock(_schedules_mtx);
 
   log_info("schedule manager set new schedules");
   log_new_schedules(sches, schedules_len);
 }
 
 int sm_start_scheduler(){
-  if (pthread_mutex_init(schedules_mtx, NULL) != 0){
+  if (pthread_mutex_init(_schedules_mtx, NULL) != 0){
     return 1;
   }
-  if(pthread_create(scheduler_thread, NULL, &_sm_run_scheduler_thread, NULL)!=0){
+  if(pthread_create(_scheduler_thread, NULL, &_sm_run_scheduler_thread, NULL)!=0){
     return 2;
   }
 
